6dc6b9a136b4e5ebe103482925acc93f37ca0bf6
Christoph Sonntag <info@christophsonntag.com>
2023-01-15 19:10:20 +0100

    Create basic items models as defined in #8

--- dad/items/models.py
+++ dad/items/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Exhibition(models.Model):
+    start_date = models.DateField(auto_now_add = False)
+    end_date = models.DateField(auto_now_add = False)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class MediaType(models.model):
+    MEDIA_TYPES = (
+        ('AUDIO', 'Audio Files'),
+        ('VIDEO', 'Video Files'),
+        ('IMAGE', 'Image Files'),
+        ('TEXT', 'Text Files')
+    )
+    description = models.TextField()
+    media_type_name = models.CharField(default='IMAGE', max_length=5, choices=MEDIA_TYPES)
+
+
+class Item(models.model):
+    related_to = models.ManyToManyField('self', blank=True, null=True)
+    part_of = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
+    type_of = models.ForeignKey(MediaType, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    digital_copy = models.FileField(upload_to='digital_copies')
+
+
+
