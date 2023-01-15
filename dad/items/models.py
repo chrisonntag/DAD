54743456e84de59d13c635baadae4ee42e785c3f
Christoph Sonntag <info@christophsonntag.com>
2023-01-15 19:25:43 +0100

    Complete schema implementation, closes #8

--- dad/items/models.py
+++ dad/items/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 
 
 class Exhibition(models.Model):
-    start_date = models.DateField(auto_now_add = False)
-    end_date = models.DateField(auto_now_add = False)
+    start_date = models.DateField(auto_now_add=False)
+    end_date = models.DateField(auto_now_add=False)
     name = models.CharField(max_length=200)
     description = models.TextField()
 
 
-class MediaType(models.model):
+class MediaType(models.Model):
     MEDIA_TYPES = (
         ('AUDIO', 'Audio Files'),
         ('VIDEO', 'Video Files'),
@@ -20,8 +21,8 @@ class MediaType(models.model):
     media_type_name = models.CharField(default='IMAGE', max_length=5, choices=MEDIA_TYPES)
 
 
-class Item(models.model):
-    related_to = models.ManyToManyField('self', blank=True, null=True)
+class Item(models.Model):
+    related_to = models.ManyToManyField('self', blank=True)
     part_of = models.ForeignKey(Exhibition, on_delete=models.CASCADE)
     type_of = models.ForeignKey(MediaType, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200)
@@ -29,4 +30,22 @@ class Item(models.model):
     digital_copy = models.FileField(upload_to='digital_copies')
 
 
+class Favorite(models.Model):
+    user = models.ForeignKey(User, related_name='favorites', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='favorites', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='likes', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='comments', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
